f38e3237a633fc4243ba46f5e0471041d485f736
mahmoodfoysal <foysalcse033@gmail.com>
2022-03-25 01:22:52 +0600

    appointment completed

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import { BrowserRouter } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
+import Appointment from './Pages/Appointment/Appointment/Appointment';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
         <Route path="/home">
           <Home></Home>
+        </Route>
+        <Route path="/appointment">
+          <Appointment></Appointment>
         </Route>
       </Switch>
       </BrowserRouter>
---
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import { Container, Typography } from '@mui/material';
+import Booking from '../Booing/Booking';
+
+const bookings = [
+    {
+        id: 1,
+        name: 'Teeth Orthodonics',
+        time: '08.00 Am - 09.00 AM',
+        space: 10
+    },
+    {
+        id: 2,
+        name: 'Cosmetic Dentistry',
+        time: '09.00 Am - 10.00 AM',
+        space: 8
+    },
+    {
+        id: 3,
+        name: 'Teeth Cleaning',
+        time: '10.00 Am - 11.00 AM',
+        space: 9
+    },
+    {
+        id: 4,
+        name: 'Teeth Orthodonics',
+        time: '11.00 Am - 12.00 Pm',
+        space: 10
+    },
+    {
+        id: 5,
+        name: 'Teeth Orthodonics',
+        time: '12.00 Pm - 01.00 PM',
+        space: 10
+    },
+    {
+        id: 6,
+        name: 'Teeth Orthodonics',
+        time: '01.00 Pm - 02.00 PM',
+        space: 10
+    },
+]
+
+const AvailableAppointments = ({date, setDate}) => {
+    return (
+        <Container>
+            <Typography variant='h4' sx={{ color: 'info.main', mb:3 }}>Available Appointments{date.toDateString()}</Typography>
+            <Grid container spacing={2}>
+            {
+                bookings.map(booking=> <Booking
+                key={booking.id}
+                booking={booking}
+                date={date}
+                ></Booking>)
+            }
+
+            </Grid>
+        </Container>
+    );
+};
+
+export default AvailableAppointments;
